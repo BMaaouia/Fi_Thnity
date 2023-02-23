5bc96dc8d32b49d8246c39f9d07bf7367e94f194
AZIZ <medaziz.allani@esprit.tn>
2023-02-23 01:36:13 +0100

    Second commit

---
+++ src/com/fithnity/main/GestionReclamation.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fithnity.main;
+import com.fithnity.controller.AfficherPersonneController;
+import com.fithnity.controller.ListData;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+/**
+ *
+ * @author MSI
+ */
+public class GestionReclamation extends Application  {
+
+   
+    private Stage primaryStage;
+    private Parent parentPage;
+   
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Gestion Reclamation");
+        
+        parentPage = FXMLLoader.load(getClass().getResource("/com/fithnity/view/Acceuil.fxml"));
+        Scene scene = new Scene(parentPage);
+        this.primaryStage.setScene(scene);
+        this.primaryStage.show();
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
